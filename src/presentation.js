d2850c7974c9e32563dab5db7b702861e5a19a2a
Kalid  Ahmed <kalid@mnkre.com>
2024-04-18 10:54:28 +0100

    updated firebase config

--- src/presentation.js
+++ src/presentation.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ref, onValue } from "firebase/database";
-import { db } from "./firebase";
+import  db  from "./firebase";
 
 function Presentation() {
   const [persons, setPersons] = useState([]);
@@ -102,7 +102,7 @@ function Presentation() {
 
   const currentPerson = persons[currentPersonIndex];
   const backgroundColor =
-    currentPerson && currentPerson.color === "red" ? "#DD0101" : "#1E196A";
+    currentPerson && currentPerson.color === "red" ? "#DD0101" : "#292667";
 
   useEffect(() => {
     document.body.style.backgroundColor = backgroundColor;
@@ -119,34 +119,96 @@ function Presentation() {
 
   const logoSrc =
     currentPerson && currentPerson.color === "red"
-      ? "/spmga.jpg"
-      : "/mnkre.jpg";
+      ? "https://mnkre.com/wp-content/uploads/2024/04/Specialty_MGA_UK_logo-removebg-preview.png"
+      : "https://mnkre.com/wp-content/uploads/2022/04/mnk-logo.png";
+
+  const shardImage =
+    currentPerson && currentPerson.color === "red"
+        ? "https://mnkre.com/wp-content/uploads/2024/02/smga-shard-1.png"
+        : "https://mnkre.com/wp-content/uploads/2024/04/shard-and-clouds-image-1-e1712069200251.png";
+ 
+  // return (
+  //   <div className="container">
+  //     <img align="left" className="mt-2 mb-0" width="250" src={logoSrc} alt="Logo" />
+  //     <div className="row justify-content-center mt-5" >
+  //       <div className="col-lg-6 col-md-8 col-sm-10 col-12 text-center" style={{marginTop:"100px", marginRight:'200px'}}>
+  //         <div>
+  //           {currentPerson ? (
+  //             <>
+  //               <p className="text-white mt-5 display-4">Welcome</p>
+  //               <div>
+  //                 <h1 className="text-white text-capitalize display-3 "  style={{ fontWeight:"bold" }}>
+  //                   {currentPerson.Name}
+  //                 </h1>
+  //                 <h1 className="text-white text-capitalize mt-3 display-3">
+  //                   {currentPerson.Company}
+  //                 </h1>
+  //               </div>
+                
+  //             </>
+  //           ) : (
+  //             <p className="text-white display-4">Welcome</p>
+  //           )}
+           
+  //         </div>
+          
+  //       </div>
+  //       <img align="" border="0" src={shardImage} alt="" title=""  width="100%" style={{marginRight:'0px'}}/>
+  //     </div>
+  
+      
+  //   </div>
+  // );
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <img style={{ marginTop:"20px" }} width="250" src={logoSrc} alt="Logo" />
-
-      {currentPerson ? (
-        <>
-          <p  style={{ color: "white", fontSize: "70px" }}>Welcome</p>
-
+    <div className="container" style={{ position: 'relative', minHeight: '100vh' }}>
+      <img align="left" className="mt-2 mb-0" width="250" src={logoSrc} alt="Logo" />
+      <div className="row justify-content-center mt-5" >
+        <div className="col-lg-6 col-md-8 col-sm-10 col-12 text-center" style={{ marginTop: "100px", marginRight: '200px' }}>
           <div>
-            <h1 style={{ color: "white", fontSize: "100px" }}>
-              {currentPerson.Name}
-            </h1>
-            <h1 style={{ color: "white", fontSize: "60px" }}>
-              {currentPerson.Company}
-            </h1>
+            {currentPerson ? (
+              <>
+                <p className="text-white mt-5 display-4">Welcome</p>
+                <div>
+                  <h1 className="text-white text-capitalize display-3 " style={{ fontWeight: "bold" }}>
+                    {currentPerson.Name} 
+                  </h1>
+                  <h1 className="text-white text-capitalize mt-2 display-3">
+                    {currentPerson.Company}
+                  </h1>
+                </div>
+  
+              </>
+            ) : (
+                <p className="text-white display-4">Welcome</p>
+              )}
+  
           </div>
-        </>
-      ) : (
-        <p style={{ color: "white", fontSize: "70px" }}>Welcome</p>
-      )}
-      {/* <button onClick={handleToggleFetchRecent}>
-      {fetchRecent ? "Show all invitees" : "Show last three invitees"}
-    </button> */}
+  
+        </div>
+      </div>
+      <img 
+        src={shardImage} 
+        alt="" 
+        title="" 
+        style={{ 
+          position: 'absolute', 
+          bottom: '0', 
+          top:'40%',
+          left: '0', 
+          width: '90%', 
+          height:'100',
+          zIndex: '-1' 
+        }} 
+      />
     </div>
   );
-}
+  
 
+  
+  
+}
+// style={{ fontWeight: "bold" , fontSize: currentPerson.Name.length > 16 ?? '10px' }}
 export default Presentation;
+// <img  className="mt-5"
+// src={shardImage} alt="" title="" width="870" style={{marignTop:'200px'}} />
