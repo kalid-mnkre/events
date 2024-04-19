7bd1a3246d1a3d1c157740c57902d2aec588a98c
Kalid  Ahmed <kalid@mnkre.com>
2024-04-19 17:10:51 +0100

    updated the presentation css

--- src/invitees.js
+++ src/invitees.js
@@ -1,289 +1,289 @@
-import React, { useState, useEffect } from "react";
-import { ref, onValue, update } from "firebase/database";
-import { db } from "./firebase";
-import DataTable from "react-data-table-component";
-import "./Invitees.css";
-
-const Invitees = () => {
-  const [invitees, setInvitees] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [attendingCount, setAttendingCount] = useState(0);
-
-  useEffect(() => {
-    const database = db;
-    const inviteesRef = ref(database, "invitees");
-
-    onValue(inviteesRef, (snapshot) => {
-      const inviteesData = snapshot.val();
-      if (inviteesData) {
-        const inviteesArray = Object.keys(inviteesData).map((key) => ({
-          id: key,
-          ...inviteesData[key],
-        }));
-        setInvitees(inviteesArray);
-        const attendingCount = inviteesArray.filter(
-          (invitee) => invitee.status === true
-        ).length;
-        setAttendingCount(attendingCount);
-      } else {
-        setInvitees([]);
-      }
-    });
-  }, []);
-
-  const handleStatusChange = async (inviteeId) => {
-    try {
-      const database = db;
-      const inviteeRef = ref(database, `invitees/${inviteeId}`); // await update(inviteeRef, { status: true }); // Wait for 5 minutes (300000 milliseconds) before updating the created_at property
-      await update(inviteeRef, {
-        status: 'pending',
-        created_at: 0,
-      });
-      setTimeout(async () => {
-        await update(inviteeRef, {
-          status: true,
-          created_at: new Date().getTime(),
-        });
-      }, 5 * 60 * 1000);
-
-      setAttendingCount(attendingCount + 1);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleStatusChangeFalse = async (inviteeId) => {
-    try {
-      const database = db;
-      const inviteeRef = ref(database, `invitees/${inviteeId}`);
-      await update(inviteeRef, {
-        status: false,
-        created_at: 0,
-      }); //toTimeString()
-
-      // Update the attending count when a status is changed
-      setAttendingCount(attendingCount - 1);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const columns = [
-    {
-      name: "Invitee ID",
-      selector: (row) => row.uuid,
-      sortable: true,
-    },
-    {
-      name: "Name",
-      selector: (row) => row.Name,
-      sortable: true,
-    },
-    {
-      name: "Company Name",
-      selector: (row) => row.Company,
-      sortable: true,
-    },
-    {
-      name: "Color",
-      selector: (row) => row.color,
-      sortable: true,
-    },
-    {
-      name: "Date",
-      selector: (row) => row.created_at,
-      sortable: true,
-    },
-    {
-      name: "Status",
-      selector: (row) => (row.status ? "Attending" : "Not Attending"),
-      sortable: true,
-    },
-    {
-      name: "Action",
-      cell: (row) =>
-        row.status === false && (
-          <button onClick={() => handleStatusChange(row.id)}>Attending</button>
-        ),
-      ignoreRowClick: true,
-      allowOverflow: true,
-      button: true,
-    },
-    {
-      name: "Action",
-      cell: (row) =>
-        row.status === true && (
-          <button onClick={() => handleStatusChangeFalse(row.id)}>
-            Not Attending
-          </button>
-        ),
-      ignoreRowClick: true,
-      allowOverflow: true,
-      button: true,
-    },
-  ];
-
-  //   const customStyles = {
-  //     header: {
-  //       style: {
-  //         backgroundColor: "#00bfff",
-  //         color: "white",
-  //         fontSize: "16px",
-  //         fontWeight: "bold",
-  //       },
-  //     },
-  //   };
-
-  const filteredInvitees = invitees.filter(
-    (invitee) =>
-      invitee.uuid
-        .toLowerCase()
-        .toString()
-        .includes(searchTerm.toLowerCase().toString()) ||
-      invitee.Name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      //invitee.Company.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      //   invitee.color.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      (invitee.status ? "attending" : "not attending")
-        .toLowerCase()
-        .includes(searchTerm.toLowerCase())
-  );
-
-  return (
-    <div className="container">
-      <div
-        className="card mt-4 mb-4 "
-        style={{ width: "25rem", height: "5rem" }}
-      >
-        <div className="card-body ">
-          <h2 className="card-title mb-4">
-            {attendingCount} invitees are attending
-          </h2>
-        </div>
-      </div>
-      <input
-        type="text"
-        placeholder="Search..."
-        onChange={(event) => setSearchTerm(event.target.value)}
-      />
-      <DataTable
-        columns={columns}
-        data={filteredInvitees}
-        noDataComponent="No invitees found"
-        pagination
-      />
-    </div>
-  );
-};
-
-export default Invitees;
-
 // import React, { useState, useEffect } from "react";
-// import { collection, onSnapshot, updateDoc, doc } from "firebase/firestore";
+// import { ref, onValue, update } from "firebase/database";
 // import { db } from "./firebase";
 // import DataTable from "react-data-table-component";
 // import "./Invitees.css";
 
 // const Invitees = () => {
-//     const [invitees, setInvitees] = useState([]);
-//     const [searchTerm, setSearchTerm] = useState("");
+//   const [invitees, setInvitees] = useState([]);
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [attendingCount, setAttendingCount] = useState(0);
 
-//     useEffect(() => {
-//         const inviteeRef = collection(db, "invitees");
-//         const unsubscribe = onSnapshot(inviteeRef, (snapshot) => {
-//             const inviteesData = snapshot.docs.map((doc) => {
-//                 return { id: doc.id, ...doc.data() };
-//             });
-//             setInvitees(inviteesData);
+//   useEffect(() => {
+//     const database = db;
+//     const inviteesRef = ref(database, "invitees");
+
+//     onValue(inviteesRef, (snapshot) => {
+//       const inviteesData = snapshot.val();
+//       if (inviteesData) {
+//         const inviteesArray = Object.keys(inviteesData).map((key) => ({
+//           id: key,
+//           ...inviteesData[key],
+//         }));
+//         setInvitees(inviteesArray);
+//         const attendingCount = inviteesArray.filter(
+//           (invitee) => invitee.status === true
+//         ).length;
+//         setAttendingCount(attendingCount);
+//       } else {
+//         setInvitees([]);
+//       }
+//     });
+//   }, []);
+
+//   const handleStatusChange = async (inviteeId) => {
+//     try {
+//       const database = db;
+//       const inviteeRef = ref(database, `invitees/${inviteeId}`); // await update(inviteeRef, { status: true }); // Wait for 5 minutes (300000 milliseconds) before updating the created_at property
+//       await update(inviteeRef, {
+//         status: 'pending',
+//         created_at: 0,
+//       });
+//       setTimeout(async () => {
+//         await update(inviteeRef, {
+//           status: true,
+//           created_at: new Date().getTime(),
 //         });
+//       }, 5 * 60 * 1000);
 
-//         return () => unsubscribe();
-//     }, []);
+//       setAttendingCount(attendingCount + 1);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-//     const handleStatusChange = async (inviteeId) => {
-//         try {
-//             const inviteeDocRef = doc(db, "invitees", inviteeId);
-//             await updateDoc(inviteeDocRef, { status: true });
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     };
+//   const handleStatusChangeFalse = async (inviteeId) => {
+//     try {
+//       const database = db;
+//       const inviteeRef = ref(database, `invitees/${inviteeId}`);
+//       await update(inviteeRef, {
+//         status: false,
+//         created_at: 0,
+//       }); //toTimeString()
 
-//     const columns = [
-//         {
-//             name: "First Name",
-//             selector: (row) => row.first_name,
-//             sortable: true,
-//         },
-//         {
-//             name: "Last Name",
-//             selector: (row) => row.last_name,
-//             sortable: true,
-//         },
-//         {
-//             name: "Company Name",
-//             selector: (row) => row.company_name,
-//             sortable: true,
-//         },
-//         {
-//             name: "Color",
-//             selector: (row) => row.color,
-//             sortable: true,
-//         },
-//         {
-//             name: "Status",
-//             selector: (row) => row.status ? "Attending" : "Not Attending",
-//             sortable: true,
-//         },
-//         {
-//             name: "Action",
-//             cell: (row) => (
-//                 row.status === false && (
-//                     <button onClick={() => handleStatusChange(row.id)}>Change Status</button>
-//                 )
-//             ),
-//             ignoreRowClick: true,
-//             allowOverflow: true,
-//             button: true,
-//         },
-//     ];
+//       // Update the attending count when a status is changed
+//       setAttendingCount(attendingCount - 1);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-//     const customStyles = {
-//         header: {
-//             style: {
-//                 backgroundColor: "#00bfff",
-//                 color: "white",
-//                 fontSize: "16px",
-//                 fontWeight: "bold",
-//             },
-//         },
-//     };
+//   const columns = [
+//     {
+//       name: "Invitee ID",
+//       selector: (row) => row.uuid,
+//       sortable: true,
+//     },
+//     {
+//       name: "Name",
+//       selector: (row) => row.Name,
+//       sortable: true,
+//     },
+//     {
+//       name: "Company Name",
+//       selector: (row) => row.Company,
+//       sortable: true,
+//     },
+//     {
+//       name: "Color",
+//       selector: (row) => row.color,
+//       sortable: true,
+//     },
+//     {
+//       name: "Date",
+//       selector: (row) => row.created_at,
+//       sortable: true,
+//     },
+//     {
+//       name: "Status",
+//       selector: (row) => (row.status ? "Attending" : "Not Attending"),
+//       sortable: true,
+//     },
+//     {
+//       name: "Action",
+//       cell: (row) =>
+//         row.status === false && (
+//           <button onClick={() => handleStatusChange(row.id)}>Attending</button>
+//         ),
+//       ignoreRowClick: true,
+//       allowOverflow: true,
+//       button: true,
+//     },
+//     {
+//       name: "Action",
+//       cell: (row) =>
+//         row.status === true && (
+//           <button onClick={() => handleStatusChangeFalse(row.id)}>
+//             Not Attending
+//           </button>
+//         ),
+//       ignoreRowClick: true,
+//       allowOverflow: true,
+//       button: true,
+//     },
+//   ];
 
-//     const filteredInvitees = invitees.filter(
-//         (invitee) =>
-//             invitee.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-//             invitee.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-//             invitee.company_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-//             invitee.color.toLowerCase().includes(searchTerm.toLowerCase()) ||
-//             (invitee.status ? "attending" : "not attending")
-//                 .toLowerCase()
-//                 .includes(searchTerm.toLowerCase())
-//     );
+//   //   const customStyles = {
+//   //     header: {
+//   //       style: {
+//   //         backgroundColor: "#00bfff",
+//   //         color: "white",
+//   //         fontSize: "16px",
+//   //         fontWeight: "bold",
+//   //       },
+//   //     },
+//   //   };
 
-//     return (
-//         <div className="container">
-//             <input
-//                 type="text"
-//                 placeholder="Search..."
-//                 onChange={(event) => setSearchTerm(event.target.value)}
-//             />
-//             <DataTable
-//                 columns={columns}
-//                 data={filteredInvitees}
-//                 noDataComponent="No invitees found"
-//                 pagination
-//             />
+//   const filteredInvitees = invitees.filter(
+//     (invitee) =>
+//       invitee.uuid
+//         .toLowerCase()
+//         .toString()
+//         .includes(searchTerm.toLowerCase().toString()) ||
+//       invitee.Name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+//       //invitee.Company.toLowerCase().includes(searchTerm.toLowerCase()) ||
+//       //   invitee.color.toLowerCase().includes(searchTerm.toLowerCase()) ||
+//       (invitee.status ? "attending" : "not attending")
+//         .toLowerCase()
+//         .includes(searchTerm.toLowerCase())
+//   );
+
+//   return (
+//     <div className="container">
+//       <div
+//         className="card mt-4 mb-4 "
+//         style={{ width: "25rem", height: "5rem" }}
+//       >
+//         <div className="card-body ">
+//           <h2 className="card-title mb-4">
+//             {attendingCount} invitees are attending
+//           </h2>
 //         </div>
-//     );
+//       </div>
+//       <input
+//         type="text"
+//         placeholder="Search..."
+//         onChange={(event) => setSearchTerm(event.target.value)}
+//       />
+//       <DataTable
+//         columns={columns}
+//         data={filteredInvitees}
+//         noDataComponent="No invitees found"
+//         pagination
+//       />
+//     </div>
+//   );
 // };
 
 // export default Invitees;
+
+// // import React, { useState, useEffect } from "react";
+// // import { collection, onSnapshot, updateDoc, doc } from "firebase/firestore";
+// // import { db } from "./firebase";
+// // import DataTable from "react-data-table-component";
+// // import "./Invitees.css";
+
+// // const Invitees = () => {
+// //     const [invitees, setInvitees] = useState([]);
+// //     const [searchTerm, setSearchTerm] = useState("");
+
+// //     useEffect(() => {
+// //         const inviteeRef = collection(db, "invitees");
+// //         const unsubscribe = onSnapshot(inviteeRef, (snapshot) => {
+// //             const inviteesData = snapshot.docs.map((doc) => {
+// //                 return { id: doc.id, ...doc.data() };
+// //             });
+// //             setInvitees(inviteesData);
+// //         });
+
+// //         return () => unsubscribe();
+// //     }, []);
+
+// //     const handleStatusChange = async (inviteeId) => {
+// //         try {
+// //             const inviteeDocRef = doc(db, "invitees", inviteeId);
+// //             await updateDoc(inviteeDocRef, { status: true });
+// //         } catch (error) {
+// //             console.error(error);
+// //         }
+// //     };
+
+// //     const columns = [
+// //         {
+// //             name: "First Name",
+// //             selector: (row) => row.first_name,
+// //             sortable: true,
+// //         },
+// //         {
+// //             name: "Last Name",
+// //             selector: (row) => row.last_name,
+// //             sortable: true,
+// //         },
+// //         {
+// //             name: "Company Name",
+// //             selector: (row) => row.company_name,
+// //             sortable: true,
+// //         },
+// //         {
+// //             name: "Color",
+// //             selector: (row) => row.color,
+// //             sortable: true,
+// //         },
+// //         {
+// //             name: "Status",
+// //             selector: (row) => row.status ? "Attending" : "Not Attending",
+// //             sortable: true,
+// //         },
+// //         {
+// //             name: "Action",
+// //             cell: (row) => (
+// //                 row.status === false && (
+// //                     <button onClick={() => handleStatusChange(row.id)}>Change Status</button>
+// //                 )
+// //             ),
+// //             ignoreRowClick: true,
+// //             allowOverflow: true,
+// //             button: true,
+// //         },
+// //     ];
+
+// //     const customStyles = {
+// //         header: {
+// //             style: {
+// //                 backgroundColor: "#00bfff",
+// //                 color: "white",
+// //                 fontSize: "16px",
+// //                 fontWeight: "bold",
+// //             },
+// //         },
+// //     };
+
+// //     const filteredInvitees = invitees.filter(
+// //         (invitee) =>
+// //             invitee.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+// //             invitee.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+// //             invitee.company_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+// //             invitee.color.toLowerCase().includes(searchTerm.toLowerCase()) ||
+// //             (invitee.status ? "attending" : "not attending")
+// //                 .toLowerCase()
+// //                 .includes(searchTerm.toLowerCase())
+// //     );
+
+// //     return (
+// //         <div className="container">
+// //             <input
+// //                 type="text"
+// //                 placeholder="Search..."
+// //                 onChange={(event) => setSearchTerm(event.target.value)}
+// //             />
+// //             <DataTable
+// //                 columns={columns}
+// //                 data={filteredInvitees}
+// //                 noDataComponent="No invitees found"
+// //                 pagination
+// //             />
+// //         </div>
+// //     );
+// // };
+
+// // export default Invitees;
--- src/presentation.js
+++ src/presentation.js
@@ -1,7 +1,184 @@
+// import React, { useState, useEffect } from "react";
+// import { ref, onValue } from "firebase/database";
+// import  db  from "./firebase";
+
+// function Presentation() {
+//   const [persons, setPersons] = useState([]);
+//   const [currentPersonIndex, setCurrentPersonIndex] = useState(0);
+//   const [loopRecent, setLoopRecent] = useState(true);
+//   const [fetchRecent, setFetchRecent] = useState(false);
+
+//   useEffect(() => {
+//     const inviteesRef = ref(db, "invitees");
+//     const unsubscribe = onValue(inviteesRef, (snapshot) => {
+//       const inviteesObject = snapshot.val();
+//       const inviteesArray = Object.values(inviteesObject)
+//         .filter((invitee) => invitee.status === true)
+//         .sort((a, b) => b.date - a.date);
+//       setPersons(inviteesArray);
+//     });
+//     return () => unsubscribe();
+//   }, []);
+
+//   useEffect(() => {
+//     let interval;
+//     if (persons.length > 0) {
+//       interval = setInterval(() => {
+//         setCurrentPersonIndex((index) =>
+//           index === persons.length - 1 ? 0 : index + 1
+//         );
+//       }, 5000);
+//     }
+//     return () => clearInterval(interval);
+//   }, [persons]);
+
+//   useEffect(() => {
+//     const allInviteesRef = ref(db, "invitees");
+//     const fetchAllInvitees = () => {
+//       const unsubscribe = onValue(allInviteesRef, (snapshot) => {
+//         const inviteesObject = snapshot.val();
+//         const inviteesArray = Object.values(inviteesObject)
+//           .filter((invitee) => invitee.status === true)
+//           .sort((a, b) => b.date - a.date);
+//         setPersons(inviteesArray);
+//         setCurrentPersonIndex(0);
+//       });
+//       return () => unsubscribe();
+//     };
+  
+//     const fetchLastThreeInvitees = () => {
+//       const lastThreeInviteesRef = ref(db, "invitees");
+//       const unsubscribe = onValue(lastThreeInviteesRef, (snapshot) => {
+//         const inviteesObject = snapshot.val();
+//         const inviteesArray = Object.values(inviteesObject)
+//           .filter((invitee) => invitee.status === true)
+//           .sort((a, b) => b.date - a.date)
+//           .slice(-30)
+//           .reverse();
+//         setPersons(inviteesArray);
+//         setCurrentPersonIndex(0);
+//       });
+//       return () => unsubscribe();
+//     };
+  
+//     const timeout = setTimeout(() => {
+//         if (fetchRecent) {
+//           fetchLastThreeInvitees();
+//           console.log("Invite Three");
+//         } else {
+//           fetchAllInvitees();
+//           console.log("InviteAll");
+//         }
+//         //setLoopRecent((prevLoopRecent) => !prevLoopRecent);
+//       }, 2 * 60 * 1000);
+    
+//       const resetFetchRecentTimeout = setTimeout(() => {
+//         setFetchRecent(false);
+//       }, 5 * 60 * 1000); // 5 minutes
+    
+//       return () => {
+//         clearTimeout(timeout);
+//         clearTimeout(resetFetchRecentTimeout);
+//       };
+//     }, [fetchRecent]);
+  
+
+//   useEffect(() => {
+//     const inviteesRef = ref(db, "invitees");
+//     const unsubscribe = onValue(inviteesRef, (snapshot) => {
+//       const inviteesObject = snapshot.val();
+
+//       const inviteesArray = Object.values(inviteesObject)
+//         .filter((invitee) => invitee.status === true)
+//         .sort((a, b) => b.date - a.date)
+//         .slice(-30)
+//         .reverse();
+//       setPersons(inviteesArray);
+//       setCurrentPersonIndex(0);
+//       setFetchRecent(true)
+//     });
+//     return () => unsubscribe();
+//   }, []);
+
+//   const currentPerson = persons[currentPersonIndex];
+//   const backgroundColor =
+//     currentPerson && currentPerson.color === "red" ? "#DD0101" : "#292667";
+
+//   useEffect(() => {
+//     document.body.style.backgroundColor = backgroundColor;
+//     return () => {
+//       document.body.style.backgroundColor = null;
+//     };
+//   }, [backgroundColor]);
+
+// //   const handleToggleFetchRecent = () => {
+// //     setFetchRecent((prevFetchRecent) => !prevFetchRecent);
+// //   };
+
+  
+
+//   const logoSrc =
+//     currentPerson && currentPerson.color === "red"
+//       ? "https://mnkre.com/wp-content/uploads/2024/04/Specialty_MGA_UK_logo-removebg-preview.png"
+//       : "https://mnkre.com/wp-content/uploads/2022/04/mnk-logo.png";
+
+//   const shardImage =
+//     currentPerson && currentPerson.color === "red"
+//         ? "https://mnkre.com/wp-content/uploads/2024/02/smga-shard-1.png"
+//         : "https://mnkre.com/wp-content/uploads/2024/04/shard-and-clouds-image-2.png";
+
+//   return (
+//     <div className="container" style={{ position: 'relative', minHeight: '100vh' }}>
+//       <img align="left" className="mt-2 mb-0" width="250" src={logoSrc} alt="Logo" />
+//       <div className="row justify-content-center mt-5" >
+//         <div className="col-lg-6 col-md-8 col-sm-10 col-12 text-center" style={{ marginTop: "100px", marginRight: '200px' }}>
+//           <div>
+//             {currentPerson ? (
+//               <>
+//                 <p className="text-white mt-5 display-4">Welcome</p>
+//                 <div>
+//                   <h1 className="text-white text-capitalize display-3 " style={{ fontWeight: "bold" }}>
+//                     {currentPerson.Name} 
+//                   </h1>
+//                   <h1 className="text-white text-capitalize mt-2 display-3">
+//                     {currentPerson.Company}
+//                   </h1>
+//                 </div>
+  
+//               </>
+//             ) : (
+//                 <p className="text-white display-4">Welcome</p>
+//               )}
+  
+//           </div>
+  
+//         </div>
+//       </div>
+//       <img 
+//         src={shardImage} 
+//         alt="" 
+//         title="" 
+//         style={{ 
+//           position: 'absolute', 
+//           bottom: '0', 
+//           top:'40%',
+//           left: '0', 
+//           width: '90%', 
+//           height:'100',
+//           zIndex: '-1' 
+//         }} 
+//       />
+//     </div>
+//   );
+  
+// }
+// export default Presentation;
+
 import React, { useState, useEffect } from "react";
 import { ref, onValue } from "firebase/database";
 import  db  from "./firebase";
-
+import smgaImage from './smga-shard.png';
+import mnkImage from './mnkre-shard.png';
 function Presentation() {
   const [persons, setPersons] = useState([]);
   const [currentPersonIndex, setCurrentPersonIndex] = useState(0);
@@ -115,7 +292,16 @@ function Presentation() {
 //     setFetchRecent((prevFetchRecent) => !prevFetchRecent);
 //   };
 
+useEffect(() => {
+  // Disable scrolling on mount
+  document.body.style.overflow = 'hidden';
+
+  // Re-enable scrolling when component unmounts
+  return () => {
+    document.body.style.overflow = 'visible';
+  };
   
+}, [backgroundColor]);
 
   const logoSrc =
     currentPerson && currentPerson.color === "red"
@@ -124,51 +310,56 @@ function Presentation() {
 
   const shardImage =
     currentPerson && currentPerson.color === "red"
-        ? "https://mnkre.com/wp-content/uploads/2024/02/smga-shard-1.png"
-        : "https://mnkre.com/wp-content/uploads/2024/04/shard-and-clouds-image-1-e1712069200251.png";
-
+        ? smgaImage
+        : mnkImage;
+console.log(shardImage);
+  // const shardImage = currentPerson && currentPerson.color === "red"
+  //       ? "
+  //       : "https://mnkre.com/wp-content/uploads/2024/04/shard-and-clouds-image-2.png";
   return (
     <div className="container" style={{ position: 'relative', minHeight: '100vh' }}>
-      <img align="left" className="mt-2 mb-0" width="250" src={logoSrc} alt="Logo" />
-      <div className="row justify-content-center mt-5" >
-        <div className="col-lg-6 col-md-8 col-sm-10 col-12 text-center" style={{ marginTop: "100px", marginRight: '200px' }}>
-          <div>
-            {currentPerson ? (
-              <>
-                <p className="text-white mt-5 display-4">Welcome</p>
-                <div>
-                  <h1 className="text-white text-capitalize display-3 " style={{ fontWeight: "bold" }}>
-                    {currentPerson.Name} 
-                  </h1>
-                  <h1 className="text-white text-capitalize mt-2 display-3">
-                    {currentPerson.Company}
-                  </h1>
-                </div>
-  
-              </>
-            ) : (
-                <p className="text-white display-4">Welcome</p>
-              )}
-  
-          </div>
-  
-        </div>
+  <img align="left" className="mt-2 mb-0" width="250" src={logoSrc} alt="Logo" />
+  <div className="row mt-5 justify-content-center">
+    <div className="col-lg-12 col-md-8 col-sm-10 col-12 align-self-center text-center" style={{ marginRight: '28%' }}>
+      <div>
+        {currentPerson ? (
+          <>
+            <p className="text-white mt-5 display-3 text-uppercase">Welcome</p>
+            <div style={{ maxWidth: '100%' }}> {/* Set maximum width to 100% */}
+              <h1 className="text-white text-uppercase display-4" style={{ fontWeight: "bold", maxWidth: '100%' }}> {/* Set maximum width to 100% */}
+                {currentPerson.Name} 
+              </h1>
+              <h1 className="text-white text-uppercase mt-2 display-3" style={{ maxWidth: '100%' }}> {/* Set maximum width to 100% */}
+                {currentPerson.Company}
+              </h1>
+            </div>
+          </>
+        ) : (
+          <p className="text-white display-4">Welcome</p>
+        )}
       </div>
-      <img 
-        src={shardImage} 
-        alt="" 
-        title="" 
-        style={{ 
-          position: 'absolute', 
-          bottom: '0', 
-          top:'40%',
-          left: '0', 
-          width: '90%', 
-          height:'100',
-          zIndex: '-1' 
-        }} 
-      />
     </div>
+  </div>
+  <img 
+    src={shardImage}
+    alt="" 
+    title="" 
+    style={{ 
+      position: 'absolute', 
+      bottom: '0', 
+      left: '0', 
+      width: '100%',
+      height: '58%',  /* Use full width of the screen */
+      zIndex: '-1' 
+    }} 
+
+
+  />
+
+  
+</div>
+
+
   );
   
 }
