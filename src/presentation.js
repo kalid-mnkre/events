1f19806c4146472f2bdb0079e459e168dbd1f951
KalidAhmed <kalid@mnkre.com>
2024-04-24 16:07:55 +0100

    updated css 4

--- src/presentation.js
+++ src/presentation.js
@@ -373,7 +373,7 @@ return (
   <div className="container" style={{ position: 'relative', minHeight: '100vh' }}>
 <img align="left" className="mt-2 mb-0" width="250" src={logoSrc} alt="Logo" />
 <div className="row mt-5 justify-content-center">
-  <div className="col-lg-12 col-md-8 col-sm-10 col-12 align-self-center text-center" style={{ marginRight: '28%',marginTop:'4%' }}>
+  <div className="col-lg-12 col-md-8 col-sm-10 col-12 align-self-center text-center" style={{ marginRight: '28%' }}>
     <div>
       {currentPerson ? (
         <>
